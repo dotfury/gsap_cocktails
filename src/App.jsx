3f420cd2a6748ce98ca09849f1c470ccc93536c8
dotfury <cm2jr4@gmail.com>
2026-04-07 16:56:50 +0900

    Art section (WIP) - 2026/04/07

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 const App = () => {
  return (
@@ -10,6 +11,7 @@ const App = () => {
     <Hero />
 		<Cocktails />
     <About />
+		<Art />
 	</main>
  )
 }
